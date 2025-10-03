303c5d499b94e12c92885c8af3a293be5c9a4a65
benny-ernst <bennyernst@u.boisestate.edu>
2025-10-03 10:25:12 -0600

    complete lab

--- full_adder.v
+++ full_adder.v
@@ -1,1 +1,10 @@
-// Implement module called full_adder
+module full_adder(
+    input A_lsb, A_msb, B_lsb, B_msb,
+    output sum_lsb, sum_msb, carryout
+);
+
+    assign sum_lsb = A_lsb ^ B_lsb;
+    assign sum_msb = A_msb ^ B_msb ^ (A_lsb & B_lsb);
+    assign carryout = (A_msb & B_msb) | (B_msb & (A_lsb & B_lsb)) | (A_msb & (A_lsb & B_lsb));
+
+endmodule
--- top.v
+++ top.v
@@ -1,1 +1,29 @@
-// Implement top level module
+module top(
+    input [7:0] sw,
+    output [5:0] led
+);
+
+    light light_inst(
+        .downstairs(sw[0]),
+        .upstairs(sw[1]),
+        .stair_light(led[0])
+    );
+
+    adder adder_inst(
+        .A(sw[2]),
+        .B(sw[3]),
+        .Y(led[1]),
+        .carry(led[2])
+    );
+    
+    full_adder full_adder_inst(
+        .A_lsb(sw[4]),
+        .A_msb(sw[5]),
+        .B_lsb(sw[6]),
+        .B_msb(sw[7]),
+        .sum_lsb(led[3]),
+        .sum_msb(led[4]),
+        .carryout(led[5])
+    );
+
+endmodule
